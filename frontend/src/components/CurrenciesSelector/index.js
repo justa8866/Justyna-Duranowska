4eb9033937f7cc0054c4bba9b48b4fd26c3153d0
Justyna Duranowska <justa8866@gmail.com>
2022-10-29 12:23:39 +0200

    fix default currency on reloading page

--- frontend/src/components/CurrenciesSelector/index.js
+++ frontend/src/components/CurrenciesSelector/index.js
@@ -2,83 +2,105 @@ import React, { Component } from "react";
 import { gql } from "@apollo/client";
 
 import {
-    Dollar,
-    Arrow,
-    DropdownMenu,
-    DropdownItem,
+  Dollar,
+  Arrow,
+  DropdownMenu,
+  DropdownItem,
 } from "./CurrenciesSelector.style";
 
 import arrow from "./arrow.png";
 
 const GET_CURRENCIES = gql`
-    query GetCurrencies {
-        currencies {
-            label
-            symbol
-        }
+  query GetCurrencies {
+    currencies {
+      label
+      symbol
     }
+  }
 `;
 
 class CurrenciesSelector extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isToggleOn: false,
-            currencies: [],
-            ActiveCurrency: new Object()
-        };
-    }
-
-    onChangeActiveCurrency = (currency) => { 
-        this.setState({ ActiveCurrency: currency, isToggleOn: false });
-        this.props.onChangeActiveCurrency(currency); 
+  constructor(props) {
+    super(props);
+    this.state = {
+      isToggleOn: false,
+      currencies: [],
+      ActiveCurrency: {},
     };
+  }
 
-    componentDidMount() {
-        this.getCurrencies();
+  onChangeActiveCurrency = (currency) => {
+    this.setState({ ActiveCurrency: currency, isToggleOn: false });
+    localStorage.setItem("currency", JSON.stringify(currency));
+    this.props.onChangeActiveCurrency(currency);
+  };
+
+  componentDidMount() {
+    this.getCurrencies();
+  }
+
+  getActiveCurrency() {
+    const currency = localStorage.getItem("currency");
+
+    if (!currency) {
+      return null;
     }
 
-    async getCurrencies() {
-        const result = await this.props.client.query({
-            query: GET_CURRENCIES,
-        });      
-        const currencies = [...this.state.currencies];
+    return JSON.parse(currency);
+  }
+
+  async getCurrencies() {
+    const result = await this.props.client.query({
+      query: GET_CURRENCIES,
+    });
+    const currencies = [...this.state.currencies];
 
     if (result.data) {
-        if (result.data.currencies) {
-            result.data.currencies.map((currency,index) => {
-                if(index === 0) {
-                    this.props.onChangeActiveCurrency(currency) 
-                }
-                currencies.push({ label: currency.label, symbol: currency.symbol });
-            });
-        }
+      if (result.data.currencies) {
+        result.data.currencies.map((currency, index) => {
+          currencies.push({ label: currency.label, symbol: currency.symbol });
+        });
+      }
     }
 
-    this.setState({ currencies, ActiveCurrency: { symbol: currencies[0].symbol, label: currencies[0].label } });
-}
+    const storageCurrency = this.getActiveCurrency();
 
-    handleClick = () => {
-        this.setState((prevState) => ({
-            isToggleOn: !prevState.isToggleOn,
-        }));
-    };
+    this.onChangeActiveCurrency(storageCurrency || currencies[0]);
 
-    render() {
-        return (
-            <>
-                <Dollar>{this.state.ActiveCurrency.symbol}</Dollar>
-                <Arrow src={arrow} onClick={this.handleClick}></Arrow>
-                <DropdownMenu toggleOn={this.state.isToggleOn}>
-                {this.state.currencies.map((currency,index) => (
-                    <DropdownItem key={index}
-                    ActiveCurrency={this.state.ActiveCurrency}
-                    onClick={() => this.onChangeActiveCurrency(currency)}
-                    >{currency.symbol} {currency.label}</DropdownItem>))}
-                </DropdownMenu>
-            </>
-        );
-    }
+    this.setState({
+      currencies,
+      ActiveCurrency: {
+        symbol: storageCurrency ? storageCurrency.symbol : currencies[0].symbol,
+        label: storageCurrency ? storageCurrency.label : currencies[0].label,
+      },
+    });
+  }
+
+  handleClick = () => {
+    this.setState((prevState) => ({
+      isToggleOn: !prevState.isToggleOn,
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <Dollar>{this.state.ActiveCurrency.symbol}</Dollar>
+        <Arrow src={arrow} onClick={this.handleClick}></Arrow>
+        <DropdownMenu toggleOn={this.state.isToggleOn}>
+          {this.state.currencies.map((currency, index) => (
+            <DropdownItem
+              key={index}
+              ActiveCurrency={this.state.ActiveCurrency}
+              onClick={() => this.onChangeActiveCurrency(currency)}
+            >
+              {currency.symbol} {currency.label}
+            </DropdownItem>
+          ))}
+        </DropdownMenu>
+      </>
+    );
+  }
 }
 
 export default CurrenciesSelector;
