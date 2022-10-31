059205287d7755f3fa8d0cc87bd2e38d4d926334
Justyna Duranowska <justa8866@gmail.com>
2022-10-31 23:47:40 +0100

    cart overlay appears on PDP

--- frontend/src/components/ProductView/ProductView.js
+++ frontend/src/components/ProductView/ProductView.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
     width: 90%;
     margin: 0px auto;
-    margin-top: 60px;
+    padding-top: 60px;
     display: grid;
     grid-gap: 1rem;
     grid-template-columns: 60% 40%;
--- frontend/src/pages/Product.js
+++ frontend/src/pages/Product.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ProductView from "../components/ProductView";
 import Navbar from "../components/Navbar";
+import { Overlay } from "./overlay.style";
 
 class Product extends Component {
   constructor(props) {
@@ -9,12 +10,22 @@ class Product extends Component {
       ActiveCurrency: {},
       ActiveCategory: "",
       cartItemsCount: 0,
+      CardListBackdropFilter: false,
+      DropDownVisibility: false,
     };
   }
 
   componentDidMount() {
     this.getCartItemsCount();
   }
+
+  onViewCardListBackdropFilter = (boolean) => {
+    this.setState({ CardListBackdropFilter: boolean });
+  };
+
+  onChangeDropDownVisibility = (boolean) => {
+    this.setState({ DropDownVisibility: boolean });
+  };
 
   onChangeActiveCurrency = (currency) => {
     this.setState({ ActiveCurrency: currency });
@@ -57,13 +68,24 @@ class Product extends Component {
           onChangeCategory={this.onChangeCategory}
           cartItemsCount={this.state.cartItemsCount}
           onChangeCartItem={this.onChangeCartItem}
+          onViewCardListBackdropFilter={this.onViewCardListBackdropFilter}
+          onChangeDropDownVisibility={this.onChangeDropDownVisibility}
+          DropDownVisibility={this.state.DropDownVisibility}
         />
-        <ProductView
-          client={this.props.client}
-          ActiveCurrency={this.state.ActiveCurrency}
-          ActiveCategory={this.state.ActiveCategory}
-          onChangeCartItem={this.onChangeCartItem}
-        />
+        <Overlay
+          onClick={() => {
+            this.onViewCardListBackdropFilter(false);
+            this.onChangeDropDownVisibility(false);
+          }}
+          CardListBackdropFilter={this.state.CardListBackdropFilter}
+        >
+          <ProductView
+            client={this.props.client}
+            ActiveCurrency={this.state.ActiveCurrency}
+            ActiveCategory={this.state.ActiveCategory}
+            onChangeCartItem={this.onChangeCartItem}
+          />
+        </Overlay>
       </>
     );
   }
